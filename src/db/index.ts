85f9321173962a50cce3416af19578e88007a1c5
Savvas <papsavas56@gmail.com>
2024-02-10 21:36:36 +0200

    idk

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,10 +1,10 @@
 import { drizzle } from "drizzle-orm/mysql2";
-import mysql from "mysql2/promise";
+import { createConnection } from "mysql2";
 import * as responses from "./schema/responses";
 
 // export const mySqlTable = mysqlTableCreator((name) => `kep_${name}`); //! uninitialized error on schemas access
 
-const connection = await mysql.createConnection(Bun.env.DATABASE_URL as string);
+const connection = createConnection(Bun.env.DATABASE_URL as string);
 const _db = drizzle(connection, { schema: { ...responses }, mode: "default" });
 
 export const db = _db;

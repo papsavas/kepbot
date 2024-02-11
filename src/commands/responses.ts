0be09d39a091c3780d66de1966f7dc518555fee6
Savvas <papsavas56@gmail.com>
2024-02-11 03:02:08 +0200

    asf

--- src/commands/responses.ts
+++ src/commands/responses.ts
@@ -82,7 +82,7 @@ export const responsesCommand = createCommand({
           ephemeral: true,
           embeds: batchesOf20.map((batch, batchIndex) =>
           ({
-            title: `Responses" ${batchesOf20.length > 1 ? ` (${batchIndex + 1}/${batchesOf20.length})` : ''}`,
+            title: `Responses ${batchesOf20.length > 1 ? ` (${batchIndex + 1}/${batchesOf20.length})` : ''}`,
             description: "List of your responses",
             color: Colors.Orange,
             author: {

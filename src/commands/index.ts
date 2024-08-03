8c130e74f68a68f06ee06537007c14df7e741faa
Savvas <papsavas56@gmail.com>
2024-08-03 11:52:01 +0300

    mute command

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,34 +1,57 @@
-import { ApplicationCommandType, ChatInputCommandInteraction, MessageContextMenuCommandInteraction, UserContextMenuCommandInteraction } from "discord.js";
+import {
+  ApplicationCommandType,
+  ChatInputCommandInteraction,
+  MessageContextMenuCommandInteraction,
+  UserContextMenuCommandInteraction,
+} from "discord.js";
 import type { BotCommand } from "~/lib/createCommand";
 import { moveMessageCtxCommand } from "./moveMessageCtx";
 import { moveMessageSlashCommand } from "./moveMessageSlash";
 import { responsesCommand } from "./responses";
+import { voteMuteCommand } from "./voteMute";
 
-export const commands = [responsesCommand, moveMessageCtxCommand, moveMessageSlashCommand] as unknown as BotCommand[];
+export const commands = [
+  responsesCommand,
+  moveMessageCtxCommand,
+  moveMessageSlashCommand,
+  voteMuteCommand,
+] as unknown as BotCommand[];
 
-export const { chatInputCommands, userCtxMenuCommands, messageCtxMenuCommands } = commands.reduce((acc, command) => {
-  switch (command.data.type) {
-    case ApplicationCommandType.ChatInput: {
-      acc.chatInputCommands.push(command as BotCommand<ChatInputCommandInteraction>)
-      return acc;
+export const {
+  chatInputCommands,
+  userCtxMenuCommands,
+  messageCtxMenuCommands,
+} = commands.reduce(
+  (acc, command) => {
+    switch (command.data.type) {
+      case ApplicationCommandType.ChatInput: {
+        acc.chatInputCommands.push(
+          command as BotCommand<ChatInputCommandInteraction>
+        );
+        return acc;
+      }
+
+      case ApplicationCommandType.User: {
+        acc.userCtxMenuCommands.push(command);
+        return acc;
+      }
+
+      case ApplicationCommandType.Message: {
+        acc.messageCtxMenuCommands.push(command);
+        return acc;
+      }
+
+      default:
+        return acc;
     }
-
-    case ApplicationCommandType.User: {
-      acc.userCtxMenuCommands.push(command)
-      return acc;
-    }
-
-    case ApplicationCommandType.Message: {
-      acc.messageCtxMenuCommands.push(command)
-      return acc;
-    }
-
-    default: return acc
+  },
+  {
+    chatInputCommands: [],
+    messageCtxMenuCommands: [],
+    userCtxMenuCommands: [],
+  } as {
+    chatInputCommands: BotCommand<ChatInputCommandInteraction>[];
+    userCtxMenuCommands: BotCommand<UserContextMenuCommandInteraction>[];
+    messageCtxMenuCommands: BotCommand<MessageContextMenuCommandInteraction>[];
   }
-
-}, { chatInputCommands: [], messageCtxMenuCommands: [], userCtxMenuCommands: [] } as {
-  chatInputCommands: BotCommand<ChatInputCommandInteraction>[],
-  userCtxMenuCommands: BotCommand<UserContextMenuCommandInteraction>[],
-  messageCtxMenuCommands: BotCommand<MessageContextMenuCommandInteraction>[],
-
-})
+);
---
+++ src/commands/voteMute.ts
@@ -0,0 +1,38 @@
+import {
+  ApplicationCommandType,
+  UserContextMenuCommandInteraction,
+  userMention,
+} from "discord.js";
+import { createCommand } from "~/lib/createCommand";
+
+const MUTE_VOTES_NEEDED = 5;
+const MUTE_VOTE_TIME = 1 * 60 * 1000;
+
+export const voteMuteCommand = createCommand({
+  data: {
+    name: "efyges",
+    type: ApplicationCommandType.User,
+  },
+  execute: async (interaction: UserContextMenuCommandInteraction, data) => {
+    if (!interaction.guild)
+      return interaction.reply({ content: "Use in guild" });
+    const interactionReply = await interaction.reply({
+      content: `${userMention(interaction.targetUser.id)} efyges`,
+      fetchReply: true,
+    });
+    await interactionReply.react("👋");
+    const collector = interactionReply.createReactionCollector({
+      filter: (reaction, user) => reaction.emoji.name === "👋",
+      time: MUTE_VOTE_TIME,
+    });
+    collector.on("collect", async (reaction, user) => {
+      if (reaction.users.cache.size >= MUTE_VOTES_NEEDED) {
+        const member = await interaction.guild!.members.fetch(
+          interaction.targetUser.id
+        );
+        await member.timeout(120, "ΚΕΠ DEMOCRACY");
+        collector.stop("Vote passed");
+      }
+    });
+  },
+});

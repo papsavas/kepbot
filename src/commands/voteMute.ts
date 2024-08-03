338d6ee2851ded5edc20b04bd61fd1d8b8b1244d
Savvas <papsavas56@gmail.com>
2024-08-03 12:13:17 +0300

    asfaga

--- src/commands/voteMute.ts
+++ src/commands/voteMute.ts
@@ -5,8 +5,8 @@ import {
 } from "discord.js";
 import { createCommand } from "~/lib/createCommand";
 
-const MUTE_VOTES_NEEDED = 2;
-const MUTE_VOTE_TIME = 1 * 60 * 1000;
+const MUTE_VOTES_NEEDED = 5;
+const MUTE_VOTE_TIME = 60 * 60 * 1000;
 
 export const voteMuteCommand = createCommand({
   data: {

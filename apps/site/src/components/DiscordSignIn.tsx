32f851c8421e09484d6d7e275a2b75053752032a
Savvas Papageorgiadis <papsavas56@gmail.com>
2024-10-14 23:47:46 +0300

    yeah yeah

--- apps/site/src/components/DiscordSignIn.tsx
+++ apps/site/src/components/DiscordSignIn.tsx
@@ -1,7 +1,14 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
 
-const url = `https://discord.com/oauth2/authorize?client_id=${process.env.DISCORD_CLIENT_ID}&response_type=code&redirect_uri=${process.env.BASE_URL}/api/auth/callback/discord&scope=identify`;
+const params = new URLSearchParams({
+  client_id: process.env.DISCORD_CLIENT_ID!,
+  response_type: "code",
+  redirect_uri: process.env.BASE_URL + "/api/auth/callback/discord",
+  scope: "identify",
+});
+
+const url = `https://discord.com/oauth2/authorize?${params.toString()}`;
 
 export async function DiscordSignIn() {
   return (

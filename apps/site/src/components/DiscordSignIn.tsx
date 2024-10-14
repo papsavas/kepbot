b5baa475226edf8ffae6487439b533049fbce2b7
Savvas Papageorgiadis <papsavas56@gmail.com>
2024-10-14 23:41:27 +0300

    fix authorize uri

--- apps/site/src/components/DiscordSignIn.tsx
+++ apps/site/src/components/DiscordSignIn.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
 
-const url = `https://discord.com/oauth2/authorize?client_id=${process.env.DISCORD_CLIENT_ID}&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Fauth%2Fcallback%2Fdiscord&scope=identify`;
+const url = `https://discord.com/oauth2/authorize?client_id=${process.env.DISCORD_CLIENT_ID}&response_type=code&redirect_uri=${process.env.BASE_URL}/api/auth/callback/discord&scope=identify`;
 
 export async function DiscordSignIn() {
   return (
